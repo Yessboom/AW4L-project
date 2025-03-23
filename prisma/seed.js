b3a13556bc620b1fb174bc2ff09433255effa042
Yessboom <Tanguy.guissard@hotmail.be>
2025-03-23 15:38:34 +0100

    Update DB with 1st round picks

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,20 +6,67 @@ const prisma = new PrismaClient();
 
 async function main() {
   try {
-    // Resolve the path to the JSON file
-    const filePath = path.resolve('public/scrappedData/prospects.json');
+    // Resolve the path to the JSON files
+    const prospectfilePath = path.resolve('public/scrappedData/prospects.json');
+    const teamfilePath = path.resolve('public/scrappedData/teams.json');
+    const draftPickfilePath = path.resolve('public/scrappedData/draftPicks.json');
 
-    // Read the JSON file
-    const players = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    // Read the JSON files
+    const players = JSON.parse(fs.readFileSync(prospectfilePath, 'utf-8'));
+    const teams = JSON.parse(fs.readFileSync(teamfilePath, 'utf-8'));
+    const draftPicks = JSON.parse(fs.readFileSync(draftPickfilePath, 'utf-8'));
 
-    // Use a transaction to ensure all data is inserted correctly
+    const doUpdatePlayer = false;
+    const doUpdateTeam = false;
+    const doUpdateDraftPick = true;
+
+    // Use a transaction to ensure all data is deleted and inserted correctly
     await prisma.$transaction(async (prisma) => {
-      for (const player of players) {
-        await prisma.player.create({
-          data: player,
-        });
+      // Delete all existing records in the DraftPick table first
+      if (doUpdateDraftPick) {
+        await prisma.draftPick.deleteMany();
+      }
+
+      // Delete all existing records in the Player table
+      if (doUpdatePlayer) {
+        await prisma.player.deleteMany();
+      }
+
+      // Delete all existing records in the Team table
+      if (doUpdateTeam) {
+        await prisma.team.deleteMany();
+      }
+
+      // Insert new player data
+      if (doUpdatePlayer) {
+        for (const player of players) {
+          await prisma.player.create({
+            data: player,
+          });
+        }
+      }
+
+      // Insert new team data
+      if (doUpdateTeam) {
+        for (const team of teams) {
+          await prisma.team.create({
+            data: team,
+          });
+        }
+      }
+
+
+      // Insert new draft pick data
+      if (doUpdateDraftPick) {
+        for (const pick of draftPicks) {
+          await prisma.draftPick.create({
+            data: pick,
+          });
+        }
       }
     });
+
+
 
     console.log('Data seeded successfully');
   } catch (error) {
